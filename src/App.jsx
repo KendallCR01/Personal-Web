24eeaf29833df0f3c84c4c7ad1721d756c72a498
KendallCR01 <kendall.tencio.paniagua@gmail.com>
2025-07-30 14:21:34 -0600

    Download resume ( PDF )

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,11 @@ import Header from './components/header';
 import Hero from './components/Hero'
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-800">
+    <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
       <Header />
       <Hero />
       <main className="p-8" id="inicio">
-        <section className="my-8" id="sobremi">
-          <h2 className="text-xl font-semibold mb-4">Sobre mí</h2>
-          <p>
-            Soy un desarrollador backend especializado en C#, .NET, Entity Framework, JWT y React.
-            Apasionado por crear soluciones sólidas y escalables.
-          </p>
-        </section>
-
-        <section className="my-8" id="proyectosssss">
-          <h2 className="text-xl font-semibold mb-4">Proyectos Destacados</h2>
-          <ul className="space-y-4">
-            <li className="p-4 border rounded-md bg-white shadow">
-              <h3 className="font-bold">Sistema de Formularios Patrimoniales</h3>
-              <p>Backend robusto con C# y múltiples servicios integrados.</p>
-            </li>
-            <li className="p-4 border rounded-md bg-white shadow">
-              <h3 className="font-bold">API para Gimnasio</h3>
-              <p>Node.js con Oracle y procedimientos almacenados.</p>
-            </li>
-          </ul>
-        </section>
-
-        <section className="my-8" id="contacto">
-          <h2 className="text-xl font-semibold mb-4">Contacto</h2>
-          <p>kendall.tencio@correo.com | LinkedIn | GitHub</p>
-        </section>
+       
       </main>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,23 @@ const Hero = () => {
       <p className="text-lg md:text-2xl text-gray-600 dark:text-gray-300 max-w-2xl mb-8">
         Desarrollador Backend apasionado por construir APIs robustas, eficientes y seguras. Bienvenido a mi portafolio.
       </p>
+      <div className="flex flex-wrap justify-center gap-4">
       <a
         href="#proyectos"
-        className="inline-block px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+        className="inline-block px-6 py-3 bg-gray-900 text-white rounded-lg hover:bg-blue-500 dark:bg-blue-500 dark:hover:bg-blue-600 transition"
       >
         Ver mis proyectos
       </a>
+       <a
+        href="/Resume.pdf"
+        download="Kendall-CV.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-block px-6 py-3 bg-gray-900 text-white rounded-lg hover:bg-blue-500 dark:bg-blue-500 dark:hover:bg-blue-600 transition"
+      >
+        Descargar CV
+      </a>
+      </div>
     </section>
   )
 }
